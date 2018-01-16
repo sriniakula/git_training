479efa9086f58e7ca5faf842d51dcf8b51317199
sriakula <sree.akula@gmail.com>
2018-01-15 19:47:47 -0500

    Initial Commit

---
+++ src/com/srini/MyMain.java
@@ -0,0 +1,12 @@
+package com.srini;
+
+public class MyMain {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("My First exposure to GIT");
+
+	}
+
+}
